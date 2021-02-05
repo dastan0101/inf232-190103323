a5640c2aeb816b08a53ee12d9baa946e20e718d9
Dastan Talipov <190103323@stu.sdu.edu.kz>
2021-02-05 12:11:38 +0600

    My name is Dastan

---
+++ Week 2/Task_1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php  
+	$x = 5;
+
+	function myTest() {
+		echo "<p>Variable x inside function is: $x</p>";
+	}
+
+	myTest();
+
+	echo "<p>Variable x outside function is: $x</p>";
+
+	?>
+</body>
+</html>
+
+
