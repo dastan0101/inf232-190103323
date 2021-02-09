554c24eb1669a49b72061122043295ef5f6fed3b
dastan0101 <190103323@stu.sdu.edu.kz>
2021-02-09 22:07:27 +0600

    Home Work 8

---
+++ HW8/HW_8.js
@@ -0,0 +1,15 @@
+function open() {
+	$(document).ready(function() {
+		$("#op").click(function() {
+			$("#advertising").slideDown();
+		})
+	})
+}
+
+function close() {
+	$(document).ready(function() {
+		$("#cl").click(function() {
+			$("#advertising").slideUp();
+		})
+	})
+}
