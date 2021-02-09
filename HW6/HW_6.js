80229d88fcafc1ef1b0daf0895b6ca98bf098c47
dastan0101 <190103323@stu.sdu.edu.kz>
2021-02-09 21:50:00 +0600

    Home Work 6

---
+++ HW6/HW_6.js
@@ -0,0 +1,12 @@
+// function info_1() {
+// 	document.getElementById("info").innerHTML = "Nikon";
+// }
+function info_2() {
+	document.getElementById("info").innerHTML = "Sony";
+}
+function info_3() {
+	document.getElementById("info").innerHTML = "Canon";
+}
+function info_4() {
+	document.getElementById("info").innerHTML = "Leica";
+}
